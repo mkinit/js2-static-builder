644578552e82091fbbb43a450b8e8dab50e2958c
mkinit <im@mkinit.com>
2020-12-20 21:34:37 +0800

    '删除第三方npm包'

--- src/entry.js
+++ src/entry.js
@@ -1,14 +1,2 @@
 //引用样式入口文件（固定）
 import './style/style.less'
-
-//引用自定义模块（demo）
-import test from './script/test.js'
-test()
-
-//引用第三方模块（demo）
-import jquery from 'jquery'
-console.log(jquery)
-
-//引用第三方模块（demo）
-import moment from 'moment'
-console.log(moment)
