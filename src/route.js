f1e83e092755d75eb359ff40c5eb450e2296dbf4
mkinit <im@mkinit.com>
2020-12-20 21:17:56 +0800

    '添加示例页面'

--- src/route.js
+++ src/route.js
@@ -2,7 +2,7 @@
 const front = []
 
 //dome页面（directory属性为打包后的目录，没有该属性默认打包在根目录）
-const admin = [{
+const demo = [{
 	name: 'menu',//菜单demo
 	title: 'menu',
 	directory: 'demo' //目录名称需要跟实际项目目录名称一致（src/view/demo/）
--- src/view/demo/menu/menu.js
+++ src/view/demo/menu/menu.js
@@ -1,4 +1,2 @@
-//多入口模式样式在这里引用，否则在样式入口文件（style.less）引用
-import './login.less'
-
-console.log('这是二级页面login的js')
+//多入口模式样式在这里引用样式，否则在样式入口文件（style.less）引用
+import './menu.less'
