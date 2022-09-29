57a2d6bb9e5410c477baded73d76b2dc87834b62
mkinit <im@mkinit.com>
2022-09-30 01:22:59 +0800

    第三方工具库使用npm安装

--- src/view/template-1/main.js
+++ src/view/template-1/main.js
@@ -1,20 +1,26 @@
-//轮播图
-import Swiper from '../../library/swiper-8.3.0/swiper-bundle.min.js'
-const swiper = new Swiper('.swiper', {
-	speed: 1000,
-	loop: true,
-	effect: 'fade',
-	autoplay: {
-		delay: 3000,
-		disableOnInteraction: false,
-		pauseOnMouseEnter: true
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true
-	},
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-})
+import QRCode from 'qrcode'
+import Swiper from 'swiper/swiper-bundle.min'
+window.onload = () => {
+	//轮播图
+	const swiper = new Swiper('.banner-2-swiper', {
+		speed: 1000,
+		loop: true,
+		effect: 'fade',
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true
+		},
+	})
+
+	//二维码生成器
+	QRCode.toDataURL(location.origin, {}, function(err, url) {
+		if (err) throw err
+		var img = document.querySelector('.qrcode-img')
+		img.src = url
+	})
+}
